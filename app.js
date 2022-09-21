9a69a715d0a61d428fe5a7d18d0c1ed310410c11
Martin Scalella <martinscalella@gmail.com>
2022-09-21 18:19:55 -0300

    2

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
+const publicPath = path.resolve (__dirname, "../public")
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/index.html'));
+    res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.listen(3000, () =>
